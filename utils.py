78996e646c3ab006dae71178b51ed6615f394870
J.Easter <295140325@qq.com>
2018-11-17 14:12:48 +0800

    utils: get_net_schedule() allows set set_off_station and arrival_station
    get_key_schedule() skip abnormal time

--- utils.py
+++ utils.py
@@ -9,15 +9,18 @@ from datetime import datetime
 import requests
 
 
-def get_net_schedule(date):
+def get_net_schedule(date, start_station, end_station):
     '''
     Get Schedule from the Internet
     :param date: the date to set off
+    :param start_station: the set off station
+    :param end_station: the arrival station
     :return: list
     '''
+
     payload = {'leftTicketDTO.train_date': date,
-               'leftTicketDTO.from_station': 'IZQ',
-               'leftTicketDTO.to_station': 'IOQ',
+               'leftTicketDTO.from_station': start_station,
+               'leftTicketDTO.to_station': end_station,
                'purpose_codes': 'ADULT'}
     url = 'https://kyfw.12306.cn/otn/leftTicket/query'
     r = requests.get(url, params=payload)
@@ -39,8 +42,13 @@ def get_key_schedule(origin_data, start_station, end_station):
     for _train in origin_data:
         ft = _train.split('|')
         if 'G' != ft[3][0]:
+            # skip if not High Speed Railway
             continue
         if ft[6] != start_station or ft[7] != end_station:
+            # skip if start station or end station doesn't match
+            continue
+        if ft[9] == '24:00':
+            # skip error time record
             continue
 
         _key_data = {
