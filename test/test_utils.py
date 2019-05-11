bc8c398661d01c0a4718e174032b03a8ca5032d6
J.Easter <295140325@qq.com>
2019-05-11 17:50:43 +0800

    test: 增加单元测试

---
+++ test/test_utils.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+# Author: JimruEaster<295140325@qq.com>
+
+import unittest
+
+from utils import *
+
+
+class TestUtils(unittest.TestCase):
+    def test_cal_interval_secs(self):
+        interval_sec = cal_interval_secs('2019-05-09 12:00:00',
+                                               '2019-05-09 11:00:00')
+        self.assertEqual(interval_sec, 3600)
+        interval_sec = cal_interval_secs('2019-05-09 11:00:00',
+                                               '2019-05-09 11:00:00')
+        self.assertEqual(interval_sec, 0)
+        interval_sec = cal_interval_secs('2019-05-09 11:00:00',
+                                               '2019-05-09 12:00:00')
+        self.assertEqual(interval_sec, -3600)
+
+
+if __name__ == '__main__':
+    unittest.main()
