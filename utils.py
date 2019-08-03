6455bb08cbb21b3f753bc8954690a68a6c62f6b7
J.Easter <295140325@qq.com>
2019-08-03 17:06:42 +0800

    utils: 将获取换乘班次的代码封装为 transfer_schedule()

--- utils.py
+++ utils.py
@@ -128,3 +128,54 @@ def calc_boarding_interval(depart_time):
         'start': start_boarding,
         'end': end_boarding
     }
+
+
+def transfer_schedule(fr_tsf_simple_schedule, tsf_to_simple_schedule, set_off_date, no_more_than, from_time,
+                      to_time):
+    """
+    过滤换乘班次
+    :param fr_tsf_simple_schedule:
+    :param tsf_to_simple_schedule:
+    :param set_off_date:
+    :param no_more_than:
+    :param from_time:
+    :param to_time:
+    :return:
+    """
+    result = []
+    for train1 in fr_tsf_simple_schedule:
+        for train2 in tsf_to_simple_schedule:
+            datetime1_start = set_off_date + ' ' + train1['start_time']
+            datetime1_end = set_off_date + ' ' + train1['end_time']
+            datetime2_start = set_off_date + ' ' + train2['start_time']
+            datetime2_end = set_off_date + ' ' + train2['end_time']
+            boarding_interval = calc_boarding_interval(datetime2_start)
+            _boarding_interval_start = set_off_date + ' ' + boarding_interval['start']
+
+            if calc_interval_secs(_boarding_interval_start, datetime1_end) / 60 <= 5:
+                continue
+            if calc_interval_secs(datetime2_end, datetime1_start) / 60 > no_more_than:
+                # if this set off time make the trip longer than the limit time,
+                # then this loop can directly break
+                # because the time will longer than current result
+                break
+            if datetime1_start <= set_off_date + ' %02d:00:00' % from_time:
+                continue
+            if datetime2_end >= set_off_date + ' %02d:00:00' % to_time:
+                continue
+
+            _res = {
+                'number1': train1['number'],
+                'number2': train2['number'],
+                'start_time1': train1['start_time'],
+                'end_time1': train1['end_time'],
+                'boarding_interval': '%s-%s' % (boarding_interval['start'], boarding_interval['end']),
+                'start_time2': train2['start_time'],
+                'end_time2': train2['end_time'],
+                'cost_time': calc_interval_secs(datetime2_end, datetime1_start) / 60
+            }
+            result.append(_res)
+
+    result = sorted(result, key=lambda s: s['start_time1'])
+    result = sorted(result, key=lambda s: s['cost_time'])
+    return result
